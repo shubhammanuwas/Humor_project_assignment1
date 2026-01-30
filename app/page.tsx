6408ed05880e417f7ac93f132348161b52c6806c
shubhammanuwas <shubham.manuwas2@gmail.com>
2026-01-30 14:44:09 -0500

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <main style={{ 
+      display: 'flex', 
+      justifyContent: 'center', 
+      alignItems: 'center', 
+      minHeight: '100vh',
+      fontSize: '2rem',
+      fontFamily: 'system-ui, sans-serif'
+    }}>
+      <h1>Hello world</h1>
+    </main>
+  )
+}
+
